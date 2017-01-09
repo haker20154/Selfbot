f0d9af1e0c1a904d8d8071a8c334b03b1aefff4b
Shahab <safaeishahab@yahoo.com>
2017-01-09 11:20:47 +0000

    No need for lot of yes now!

--- launch.sh
+++ launch.sh
@@ -2,34 +2,34 @@
 wget "https://valtman.name/files/telegram-cli-1222"
 mv telegram-cli-1222 tg
 chmod +x tg
-sudo apt-get install libreadline-dev
-sudo apt install libreadline-dev 
-sudo apt-get install libreadline6 
-sudo apt-get install libreadline6-dev
-sudo apt-get install libconfig-dev 
-sudo apt-get install libssl-dev 
-sudo apt-get install lua5.2 
-sudo apt-get install liblua5.2-dev 
-sudo apt-get install lua-socket 
-sudo apt-get install lua-sec 
-sudo apt-get install lua-expat 
-sudo apt-get install libevent-dev 
-sudo apt-get install make 
-sudo apt-get install unzip 
-sudo apt-get install git 
-sudo apt-get install redis-server 
-sudo apt-get install autoconf 
-sudo apt-get install g++ 
-sudo apt-get install libjansson-dev 
-sudo apt-get install libpython-dev 
-sudo apt-get install expat libexpat1-dev 
-sudo apt-get install ppa-purge 
-sudo apt-get install python3-pip 
-sudo apt-get install python3-dev
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo apt-get install gcc-6
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install libreadline-dev -y
+sudo apt install libreadline-dev -y
+sudo apt-get install libreadline6 -y
+sudo apt-get install libreadline6-dev -y
+sudo apt-get install libconfig-dev -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install lua5.2 -y
+sudo apt-get install liblua5.2-dev -y
+sudo apt-get install lua-socket -y
+sudo apt-get install lua-sec -y
+sudo apt-get install lua-expat -y
+sudo apt-get install libevent-dev -y
+sudo apt-get install make -y
+sudo apt-get install unzip -y-
+sudo apt-get install git -y
+sudo apt-get install redis-server -y
+sudo apt-get install autoconf -y
+sudo apt-get install g++ -y
+sudo apt-get install libjansson-dev -y
+sudo apt-get install libpython-dev -y
+sudo apt-get install expat libexpat1-dev -y
+sudo apt-get install ppa-purge -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-dev -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install python-software-properties -y
+sudo apt-get install gcc-6 -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
@@ -42,8 +42,8 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  cd luarocks-2.2.2
  ./configure; sudo make bootstrap
  cd ..
-sudo luarocks install serpent
-sudo luarocks install redis-lua
+sudo luarocks install serpent -y
+sudo luarocks install redis-lua -y
 chmod 777 tg
 RED='\033[0;31m'
 NC='\033[0m'
